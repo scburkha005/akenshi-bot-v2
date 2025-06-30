220e820040c13857bce2f829368d201380358ba7
scburkha005 <scburkha005@gmail.com>
2025-06-30 14:59:08 -0700

    renamed fn to getUserToken and added an api call for receiving App Access Token

--- bot-main/api/token.js
+++ bot-main/api/token.js
@@ -1,8 +1,21 @@
 const axios = require('axios');
 const { TWITCH_CLIENT_ID, TWITCH_SECRET, SESSION_SECRET, STATE_STRING } = process.env;
 // TWITCH API REQUESTS
-// POST oauth2 token
-async function getToken (code) {
+async function getAppToken () {
+	try {
+		const { data } = await axios.post('https://id.twitch.tv/oauth2/token', {
+			client_id: TWITCH_CLIENT_ID,
+			client_secret: TWITCH_SECRET,
+			grant_type: 'client_credentials'
+		})
+		console.log(data);
+		return data;
+	} catch (err) {
+		console.log(err);
+	}
+}
+// POST oauth2 token => uses code from authorize endpoint to generate user access token
+async function getUserToken (code) {
 	try {
 		const { data } = await axios.post('https://id.twitch.tv/oauth2/token', {
 			client_id: TWITCH_CLIENT_ID,
@@ -45,7 +58,7 @@ async function getUser() {
 }
 
 module.exports = {
-    getToken,
+    getUserToken,
     validateToken,
     getUser
 }
