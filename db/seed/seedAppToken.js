915457443b234611aeebc91fd852c943beb88f01
scburkha005 <scburkha005@gmail.com>
2025-06-30 17:03:36 -0700

    finished fn to fetch app access token and store in db inside of bot user

--- db/seed/seedAppToken.js
+++ db/seed/seedAppToken.js
@@ -1,14 +1,18 @@
 const { client } = require('../index.js');
 const { getAppToken } = require('../../bot-main/api/token.js');
+const { updateUser } = require('../adapters/users.js');
 require('dotenv').config({ path: '../../.env' });
 const { TWITCH_CLIENT_ID, TWITCH_SECRET } = process.env;
 
 async function seedAppAccessToken () {
   try {
-    let appAccessToken = await getAppToken(TWITCH_CLIENT_ID, TWITCH_SECRET);
-    console.log(appAccessToken);
+    let { access_token: appAccessToken } = await getAppToken(TWITCH_CLIENT_ID, TWITCH_SECRET);
+    let user = await updateUser("1265515088", {
+      appAccessToken
+    })
+    console.log("finished updating user", user);
   } catch (err) {
-    console.log(err);
+    console.log('error seeding app access token', err);
   }
 }
 
