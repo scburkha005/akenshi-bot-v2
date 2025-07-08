2d0918db2e1c11c1c5872d7e0d6f9ecbd5138b7b
scburkha005 <scburkha005@gmail.com>
2025-07-07 23:32:28 -0700

    adjusted path for env file

--- db/seed/seedAppToken.js
+++ db/seed/seedAppToken.js
@@ -1,11 +1,12 @@
 const { client } = require('../index.js');
 const { getAppToken } = require('../../bot-main/api/token.js');
 const { updateUser } = require('../adapters/users.js');
-require('dotenv').config({ path: '../../.env' });
+require('dotenv').config({ path: '../../../../../etc/akenshi-bot.env' });
 const { TWITCH_CLIENT_ID, TWITCH_SECRET } = process.env;
 
 async function seedAppAccessToken () {
   try {
+    console.log(TWITCH_CLIENT_ID, TWITCH_SECRET)
     let { access_token: appAccessToken } = await getAppToken(TWITCH_CLIENT_ID, TWITCH_SECRET);
     let user = await updateUser("1265515088", {
       appAccessToken
