d2492ea6c9c3687be877f452688c1a78481b14c9
scburkha005 <scburkha005@gmail.com>
2025-08-18 17:09:51 -0700

    basic frontend setup complete

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,12 @@ import './App.css'
 import { useState } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import { Navbar, Home } from './components';
-import reactLogo from './assets/react.svg'
 
 function App () {
-  const [count, setCount] = useState(0)
 
   return (
     <div>
-      {/* <Navbar /> */}
+      <Navbar />
       <Routes>
         <Route path='/' element={<Home />}/>
       </Routes>
