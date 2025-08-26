ee42c5a056536a34b971cd96dbefc3e6d4cd3b0f
scburkha005 <scburkha005@gmail.com>
2025-08-26 15:29:37 -0700

    prop drilled token setter and created useEffect to set token in local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
 import './App.css'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import { Navbar, Home, Login, Register } from './components';
 
 function App () {
+  const [ token, setToken ] = useState('');
+
+  // store token in local storage if it exists
+  useEffect(() => {
+    if (token) {
+      localStorage.setItem('token', token);
+    }
+  }, [token])
 
   return (
     <div className='app'>
       <Navbar />
       <Routes>
         <Route path='/' element={<Home />}/>
-        <Route path='/login' element={<Login />}/>
-        <Route path='/register' element={<Register />}/>
+        <Route path='/login' element={<Login setToken={setToken} />}/>
+        <Route path='/register' element={<Register setToken={setToken} />}/>
       </Routes>
     </div>
   )
