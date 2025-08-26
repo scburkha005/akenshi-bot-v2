bdfa45b8cce290170d5dd1a82a29d071de7843a5
scburkha005 <scburkha005@gmail.com>
2025-08-26 14:45:17 -0700

    Added routes for login and register

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import './App.css'
 import { useState } from 'react'
 import { Route, Routes } from 'react-router-dom'
-import { Navbar, Home } from './components';
+import { Navbar, Home, Login, Register } from './components';
 
 function App () {
 
   return (
-    <div>
+    <div className='app'>
       <Navbar />
       <Routes>
         <Route path='/' element={<Home />}/>
+        <Route path='/login' element={<Login />}/>
+        <Route path='/register' element={<Register />}/>
       </Routes>
     </div>
   )
