c967c694ca84b8a73a503364966817752faf991e
scburkha005 <scburkha005@gmail.com>
2025-08-26 14:45:54 -0700

    added re-exports

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,4 @@
 export { default as Home } from './Home';
 export { default as Navbar } from "./Navbar";
+export { default as Login } from "./Login";
+export { default as Register } from "./Register";
