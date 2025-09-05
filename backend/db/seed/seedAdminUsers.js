ca54dfa2f8c9c7c415c3ce57ad68fba4cb6b54b2
scburkha005 <scburkha005@gmail.com>
2025-09-04 17:48:43 -0700

    created file to seed initial admin users

---
+++ backend/db/seed/seedAdminUsers.js
@@ -0,0 +1,38 @@
+import { createAdminUser } from "../adapters/users.js";
+import client from '../index.js';
+import readline from 'node:readline';
+
+const adminUser = {
+  username: '',
+  password: '',
+  twitchUserId: '',
+  twitchDisplayName: '',
+  userAccessToken: '',
+  appAccessToken: '',
+  refreshToken: '',
+  scopes: []
+};
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question("Input username:", username => {
+  adminUser.username = username;
+  rl.question("Input password:", password => {
+    adminUser.password = password;
+    seedAdminUsers();
+    rl.close();
+  });
+});
+
+
+async function seedAdminUsers () {
+  try {
+    await createAdminUser(adminUser);
+    await client.close();
+  } catch (err) {
+    console.log(err);
+  }
+}
