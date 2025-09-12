0027a2fee9cd5c91de93be3fd7a32d790e0b53ad
scburkha005 <scburkha005@gmail.com>
2025-09-11 18:04:20 -0700

    route to create link between twitch bot account and app/db

--- backend/api/twitch.js
+++ backend/api/twitch.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import { configDotenv } from 'dotenv';
-import { getUserToken, validateToken } from '../modules/token.js';
-import { requireUser } from './modules/requireUser.js';
-import { updateUser } from '../db/adapters/users.js';
+import { getAppToken, getUserToken, validateToken } from '../modules/token.js';
+import { requireUser, requireAdminUser } from './modules/requireUser.js';
+import { createUser, findUserByUsername, updateUser } from '../db/adapters/users.js';
 configDotenv();
 const { STATE_STRING } = process.env;
 const twitchRouter = express.Router();
@@ -24,6 +24,48 @@ twitchRouter.post('/accountLink', requireUser, async function (req, res, next) {
     // start with createChatSubscription
 
     res.send(updatedUser);
+  } else {
+    next({
+      error: "Unauthorized request",
+      reason: "State string doesn't match"
+    })
+  }
+});
+
+twitchRouter.post('/botLink', requireAdminUser, async function (req, res, next) {
+	// Store code upon validation
+  if (STATE_STRING === req.body.state) {
+    const data = await getUserToken(req.body.code);
+    const userData = await validateToken(data.access_token);
+    const appToken = await getAppToken();
+
+    let akenshi_bot = await findUserByUsername('akenshi__bot');
+    if (akenshi_bot) {
+      akenshi_bot = await updateUser('akenshi__bot', {
+        twitchUserId: userData.user_id,
+        twitchDisplayName: userData.login,
+        userAccessToken: data.access_token,
+        appAccessToken: appToken,
+        refreshToken: data.refresh_token,
+        scopes: data.scope
+      });
+    } else {
+      akenshi_bot = await createUser({
+        username: 'akenshi__bot',
+        password: null,
+        isAdmin: false,
+        twitchUserId: userData.user_id,
+        twitchDisplayName: userData.login,
+        userAccessToken: data.access_token,
+        appAccessToken: appToken,
+        refreshToken: data.refresh_token,
+        scopes: data.scope
+      });
+    }
+    // continue to create BASELINE necessary events
+    // start with createChatSubscription
+
+    res.send(akenshi_bot);
   } else {
     next({
       error: "Unauthorized request",
