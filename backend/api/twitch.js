96855c510a7a100e2d05068ea121ca8cbb68b67e
scburkha005 <scburkha005@gmail.com>
2025-09-26 18:49:27 -0700

    added eventsub post and get route

--- backend/api/twitch.js
+++ backend/api/twitch.js
@@ -3,9 +3,57 @@ import { configDotenv } from 'dotenv';
 import { getAppToken, getUserToken, validateToken } from '../modules/token.js';
 import { requireUser, requireAdminUser } from './modules/requireUser.js';
 import { createBotUser, findUserByUsername, updateUser } from '../db/adapters/users.js';
+import { createHmac, verifySignatures } from './modules/hmac.js';
+import { sendMessage } from './modules/eventsub.js';
+import { getEventSubscriptions } from './modules/subscriptions.js';
 configDotenv();
-const { STATE_STRING } = process.env;
+const { HMAC_SECRET, STATE_STRING } = process.env;
 const twitchRouter = express.Router();
+
+// GET /api/twitch/eventsub
+twitchRouter.get('/eventsub', requireUser, async (req, res, next) => {
+  try {
+    let data = await getEventSubscriptions()
+    res.send(data);
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+})
+
+// POST /api/twitch/eventsub
+twitchRouter.post('/eventsub', (req, res) => {
+  // Notification Events
+  // Create our own HMAC sig to compare our signature to the one provided by twitch
+  const HMAC_MSG = `${req.headers["twitch-eventsub-message-id"]}${req.headers["twitch-eventsub-message-timestamp"]}${req.body}`
+  let HMAC_SIG = 'sha256=' + createHmac(HMAC_SECRET, HMAC_MSG);
+  if(verifySignatures(HMAC_SIG, req.headers['twitch-eventsub-message-signature'])) {
+    let notification = JSON.parse(req.body);
+    // Handle notification
+    if (req.headers["twitch-eventsub-message-type"] === 'notification') {
+      console.log('notification running')
+      console.log(notification)
+      // Captures message events
+      if (notification.subscription.type === 'channel.chat.message') {
+        sendMessage( 'placeholder', 'placeholder', `${notification.event.broadcaster_user_name} said ${notification.event.message.text}`);
+      }
+      res.sendStatus(204);
+    } else if (req.headers["twitch-eventsub-message-type"] = 'webhook_callback_verification') {
+      console.log('webhook callback verification running')
+      // Respond to the challenge request to enable our subscription
+      res.set('Content-Type', 'text/plain').status(200).send(notification.challenge);
+    } else if (req.headers["twitch-eventsub-message-type"] = 'revocation') {
+      res.sendStatus(204);
+      
+      console.log(`${notification.subscription.type} notifications revoked`);
+      console.log(`reason: ${notification.subscription.status}`);
+      console.log(`condition: ${JSON.stringify(notification.subscription.condition, null, 4)}`);
+    }
+  } else {
+    console.log('There was an issue matching signatures: Signature verification returned false');
+    res.sendStatus(403);
+  }
+});
 
 // POST /api/twitch/accountLink
 twitchRouter.post('/accountLink', requireUser, async function (req, res, next) {
