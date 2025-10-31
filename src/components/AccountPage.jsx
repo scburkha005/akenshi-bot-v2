981a187f9eb656dc78b7328526c01c3ce9b6d452
scburkha005 <scburkha005@gmail.com>
2025-10-31 12:58:54 -0700

    removed functions that we're breaking operation, component requires reformatting

--- src/components/AccountPage.jsx
+++ src/components/AccountPage.jsx
@@ -1,12 +1,12 @@
 import { useState } from 'react';
-import { getCurrentEventSubs } from '../api/twitch';
 
 function AccountPage ({ token }) {
   const [ eventSubs, setEventSubs ] = useState()
 
   async function handleEventSubs () {
     try {
-      const eventSubs = await getCurrentEventSubs(token)
+      // we need to write a function that gets only the users eventsubs
+      // const eventSubs = await getCurrentEventSubs(token)
       console.log(eventSubs)
     } catch (err) {
       console.log(err);
