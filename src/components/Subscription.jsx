6158b7a9d358e06a5e0d85c3a3411524261c30bd
scburkha005 <scburkha005@gmail.com>
2025-10-31 16:02:38 -0700

    component to render and handle delete of single subscriptions

---
+++ src/components/Subscription.jsx
@@ -0,0 +1,33 @@
+import { Card, CardContent, Typography, CardActions, Button } from '@mui/material';
+import { deleteEventSubById } from '../api/twitch';
+import { useContext } from 'react';
+import { AuthContext } from '../App';
+function Subscription ({ subscription, updateSubscriptions }) {
+  const { token } = useContext(AuthContext);
+
+  async function handleClick () {
+    try {
+      await deleteEventSubById(subscription.id, token);
+      updateSubscriptions(subscription.id);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  return (
+    <Card>
+      <CardContent sx={{ minWidth: 275 }}>
+        <Typography>
+          {subscription.condition.twitchUsername}
+        </Typography>
+        <Typography>
+          {subscription.type}
+        </Typography>
+        <CardActions>
+          <Button onClick={handleClick}>Delete Subscription</Button>
+        </CardActions>
+      </CardContent>
+    </Card>
+  )
+}
+
+export default Subscription;
