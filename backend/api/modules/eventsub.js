319dcb5312a8598ee11a729cbc4db36fab19e859
scburkha005 <scburkha005@gmail.com>
2025-11-01 15:26:15 -0700

    reformatted and added necessary imports for functionality

--- backend/api/modules/eventsub.js
+++ backend/api/modules/eventsub.js
@@ -1,19 +1,22 @@
+import { findUserByUsername } from "../../db/adapters/users.js";
+import axios from 'axios';
+const { TWITCH_CLIENT_ID } = process.env;
 // POST Send Chat Message
-export async function sendMessage (user, bot, message) {
+export async function sendMessage (broadcasterId, message) {
 	try {
-    const akenshiBot = await findUserByTwitchId("1265515088");
-		const { data } = await axios.post("https://api.twitch.tv/helix/chat/messages", {
-        "broadcaster_id": "187093318",
-        "sender_id": akenshiBot.userId,
+    const akenshiBot = await findUserByUsername("akenshi__bot");
+    // Api call returns information on whether the message was successfully sent or not
+		await axios.post("https://api.twitch.tv/helix/chat/messages", {
+        "broadcaster_id": broadcasterId,
+        "sender_id": akenshiBot.twitchUserId,
         "message": message
 			}, {
         headers: {
-          'Authorization': `Bearer ${akenshiBot.userAccessToken}`,
+          'Authorization': `Bearer ${akenshiBot.appAccessToken.token}`,
           'Client-Id': TWITCH_CLIENT_ID,
           'Content-Type': 'application/json'
         }
       })
-    console.log(data.data)
 	} catch (err) {
 		console.log('error while sending message', err.response.data)
 	}
