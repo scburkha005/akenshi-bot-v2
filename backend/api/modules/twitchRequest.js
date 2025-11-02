481b02073cdecc4572342a52675b1b4cc263f6b1
scburkha005 <scburkha005@gmail.com>
2025-11-02 13:51:21 -0800

    moved file location and created getUsers api request

---
+++ backend/api/modules/twitchRequest.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+const { TWITCH_CLIENT_ID } = process.env;
+
+export async function getAdditionalUserInfo (userId, userAccessToken) {
+  try {
+    // destructuring nested structure to get to the actual "data"
+    const { data: { data: [ data ] } } = await axios.get(`https://api.twitch.tv/helix/users?id=${userId}`, {
+      headers: {
+        'Authorization': `Bearer ${userAccessToken}`,
+        'Client-Id': TWITCH_CLIENT_ID
+      }
+    });
+    return [data.login, data.display_name];
+  } catch (err) {
+    console.log('error while fetching user info from twitch');
+    console.log(err)
+    throw err;
+  }
+}
