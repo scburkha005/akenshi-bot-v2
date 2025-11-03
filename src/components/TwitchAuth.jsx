fdbf5395215dd7c37b87af6007702a0d928e6202
scburkha005 <scburkha005@gmail.com>
2025-11-03 14:30:09 -0800

    modified scope

--- src/components/TwitchAuth.jsx
+++ src/components/TwitchAuth.jsx
@@ -2,7 +2,7 @@ const { VITE_TWITCH_CLIENT_ID, VITE_STATE_STRING, VITE_BASE_URL } = import.meta.
 
 function TwitchAuth () {
   // Input scopes into the array following the format of the twitch api docs, code will reformat to a params readable format
-  let scopes = ['channel:manage:polls', 'channel:read:polls', 'channel:bot', 'moderation:read'].join("+").replace(":", "%3A");
+  let scopes = ['channel:manage:polls', 'channel:read:polls', 'channel:bot', 'moderation:read', "moderator:manage:shoutouts"].join("+").replace(":", "%3A");
   let params = ['response_type=code', `&client_id=${VITE_TWITCH_CLIENT_ID}`, `&redirect_uri=${VITE_BASE_URL}`, `&scope=${scopes}`, `&state=${VITE_STATE_STRING}`]
   params = params.join('');
   
