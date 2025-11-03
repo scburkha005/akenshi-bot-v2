84d0b860e9e795b5ee072e4db54986be2e30c6f2
scburkha005 <scburkha005@gmail.com>
2025-11-03 14:29:36 -0800

    auto shoutout on raid now sends a twitch shoutout as well

--- backend/api/botFunctionality/raidBehavior.js
+++ backend/api/botFunctionality/raidBehavior.js
@@ -1,18 +1,21 @@
 import { sendMessage } from "../modules/eventsub.js";
 import { findUserByTwitchId } from "../../db/adapters/users.js";
-import { getChannelInformation } from "../modules/twitchRequest.js";
+import { getChannelInformation, sendShoutout } from "../modules/twitchRequest.js";
 
 const raidChatShoutout = async (broadcaster, notification) => {
   try {
     const { autoShoutoutRaid, gtotMode } = broadcaster.botSettings.toggle;
     if (autoShoutoutRaid) {
       let raider = notification.event.from_broadcaster_user_name;
+      let broadcasterId = notification.event.to_broadcaster_user_id;
+      let raiderId = notification.event.from_broadcaster_user_id;
       if (gtotMode && broadcaster.botSettings.gtotModeEnabled) {
-        await sendMessage(notification.event.to_broadcaster_user_id, `Yo shoutout my dawg ${raider}`);
+        await sendMessage(broadcasterId, `Yo shoutout my dawg ${raider}`);
         return;
       }
-      const { game_name } = await getChannelInformation(notification.event.from_broadcaster_user_id);
-      await sendMessage(notification.event.to_broadcaster_user_id, `Check out ${raider}, they are playing ${game_name} at https://twitch.tv/${raider.toLowerCase()}`);
+      const { game_name } = await getChannelInformation(raiderId);
+      await sendMessage(broadcasterId, `Check out ${raider}, they are playing ${game_name} at https://twitch.tv/${raider.toLowerCase()}`);
+      await sendShoutout(broadcasterId, raiderId, broadcaster.userAccessToken.token);
     }
   } catch (err) {
     console.log('error while shouting out raid in chat')
