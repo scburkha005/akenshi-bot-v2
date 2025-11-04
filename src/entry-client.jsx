481ed03643693ce22f17962ef2672947a8f193d7
scburkha005 <scburkha005@gmail.com>
2025-11-04 13:12:32 -0800

    moved theme to hydrate level, restructured App to a better format for mui

--- src/entry-client.jsx
+++ src/entry-client.jsx
@@ -3,12 +3,31 @@ import { StrictMode } from 'react'
 import { hydrateRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router'
 import { App } from './App'
+import { ThemeProvider, createTheme, CssBaseline } from '@mui/material'
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#90caf9',
+    },
+    secondary: {
+      main: '#ce93d8',
+    },
+    background: {
+      default: '#121212',
+      paper: '#121212',
+    },
+  },
+});
 
 hydrateRoot(
   document.getElementById('root'),
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ThemeProvider theme={darkTheme}>
+      <BrowserRouter>
+        <CssBaseline />
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
   </StrictMode>,
 )
