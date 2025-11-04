37e2932af8e2c01dcb20cc60f19547171a6c9025
scburkha005 <scburkha005@gmail.com>
2025-11-04 13:28:38 -0800

    added box for flexbox

--- src/components/AdminPage.jsx
+++ src/components/AdminPage.jsx
@@ -1,14 +1,14 @@
 import BotTwitchAuth from './BotTwitchAuth';
 import Subscriptions from './Subscriptions';
-import { Typography } from '@mui/material';
+import { Typography, Box } from '@mui/material';
 function AdminPage () {
 
   return (
-    <>
+    <Box sx={{display: 'flex', flexDirection: "column", alignItems: "center"}}>
       <Typography variant="h4" sx={{textAlign: "center", py: 1}}>Admin page</Typography> 
       <Subscriptions />
       <BotTwitchAuth />
-    </>
+    </Box>
   );
 }
 
