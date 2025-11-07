54a2e8b0b10ea860ea512d1d992b627badb7b31d
scburkha005 <scburkha005@gmail.com>
2025-11-07 11:13:32 -0800

    restructured, moved logging to its own file and added to logging in order to handle both message and raid type events

---
+++ backend/api/botFunctionality/loggingBehavior.js
@@ -0,0 +1,42 @@
+import { createEventSubscriptionLog } from '../../db/adapters/eventSubs.js';
+export const eventsubLogHandler = async (notification, broadcasterId) => {
+  try {
+    let subscriptionType = notification.subscription.type;
+    let isDuplicateEvent = await duplicateEventHandler(broadcasterId, notification, subscriptionType)
+    // If we hit a duplicate event, return immediately
+    if (isDuplicateEvent) {
+      return isDuplicateEvent;
+    }
+    // Log subscriptions, different subscription types may require different identifiers to be logged
+    if (subscriptionType === 'channel.chat.message') {
+      await createEventSubscriptionLog(broadcasterId, notification.event.message_id);
+    } else if (subscriptionType === 'channel.raid') {
+      await createEventSubscriptionLog(broadcasterId, notification.event.from_broadcaster_user_id);
+    }
+
+    return isDuplicateEvent;
+  } catch (err) {
+    console.log('error while logging subscriptions');
+    throw err;
+  }
+}
+
+export const duplicateEventHandler = async (broadcasterId, notification, subscriptionType) => {
+  try {
+    let eventsubLog = await getEventSubscriptionLogByBroadcasterId(broadcasterId);
+    if (subscriptionType === 'channel.chat.message') {
+      if (eventsubLog.includes(notification.event.message_id)) {
+        return true;
+      }
+    }
+    if (subscriptionType === 'channel.raid') {
+      if (eventsubLog.includes(notification.event.from_broadcaster_user_id)) {
+        return true;
+      }
+    }
+    return false;
+  } catch (err) {
+    console.log('error while checking duplicate events');
+    throw err;
+  }
+}
