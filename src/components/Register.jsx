83a50855e6f13e716afa519c10088d1ae031365b
scburkha005 <scburkha005@gmail.com>
2025-11-13 19:05:03 -0800

    refactored Register to use Mui components, reformatted state to be handled on change

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,17 +1,31 @@
-import { Link, useNavigate } from "react-router";
+import { useNavigate } from "react-router";
 import { useState, useContext } from "react";
 import { AuthContext } from "../App.jsx";
 import { register } from "../api/user.js";
+import { Paper, Box, Typography, FormControl, FormLabel, OutlinedInput, Button, Link } from '@mui/material';
 
 function Register () {
   const { setToken } = useContext(AuthContext);
   const navigate = useNavigate();
-  let [error, setError] = useState({});
+  const [ username, setUsername ] = useState('');
+  const [ password, setPassword ] = useState('');
+  const [ passwordConfirm, setPasswordConfirm ] = useState('');
+  const [error, setError] = useState({});
+  // Storing references to state setters in an object in order to call them modularly based on string for onChange
+  const setters = {
+    setUsername,
+    setPassword,
+    setPasswordConfirm
+  }
+
+  function onChange (e) {
+    let fieldType = e.target.id
+    setters[`set${fieldType}`](e.target.value);
+  }
 
   async function onRegister (e) {
     e.preventDefault();
-    let [{ value: username }, { value: password}, { value: passwordRepeat }] = e.target
-    if (password !== passwordRepeat) {
+    if (password !== passwordConfirm) {
       setError({
         error: "Failed to create account",
         reason: "Passwords do not match"
@@ -28,17 +42,48 @@ function Register () {
   }
   return (
     <>
+    <Paper variant='outlined' sx={{
+      p: 3,
+      backgroundColor: "#202020",
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center'
+    }}>
       {error?.error && <div id='error-message'>{`${error.error}: ${error.reason}`}</div>}
-      <form id="login-form" onSubmit={onRegister}>
-        <label>Username: </label>
-        <input />
-        <label>Password: </label>
-        <input />
-        <label>Confirm Password: </label>
-        <input />
-        <button type="submit">Register</button>
-      </form>
-      <Link to='/login'>Already have an account? Login here</Link>
+      <Typography variant='h4'>Sign Up</Typography>
+      <Box component="form" onSubmit={onRegister} sx={{
+        display: "flex",
+        flexDirection: 'column',
+        p: 2
+      }}>
+        <FormControl>
+          <FormLabel variant='standard' for="Username">Username</FormLabel>
+          <OutlinedInput id="Username" onChange={onChange} sx={{
+            height: '2.5rem',
+            mt: 1,
+            mb: 2
+          }}/>
+        </FormControl>
+        <FormControl>
+          <FormLabel variant='standard' for="Password">Password</FormLabel>
+          <OutlinedInput id="Password" type='password' onChange={onChange} sx={{
+            height: '2.5rem',
+            mt: 1,
+            mb: 2
+          }}/>
+        </FormControl>
+        <FormControl>
+          <FormLabel variant='standard' for="PasswordConfirm">Confirm Password</FormLabel>
+          <OutlinedInput id="PasswordConfirm" type='password' onChange={onChange} sx={{
+            height: '2.5rem',
+            mt: 1,
+            mb: 2
+          }}/>
+        </FormControl>
+        <Button variant='contained' type="submit">Register</Button>
+      </Box>
+      <Link to='/login'>Already have an account? Sign in here</Link>
+    </Paper>
     </>
   )
 }
