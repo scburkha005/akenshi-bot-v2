10ad35c9d8e55a68ed402de1de368b159ef9c0cf
scburkha005 <scburkha005@gmail.com>
2025-11-13 18:35:29 -0800

    various css and design changes

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,7 +2,7 @@ import { login } from '../api/user.js';
 import { useState, useContext } from 'react';
 import { AuthContext } from '../App.jsx';
 import { useNavigate } from 'react-router';
-import { Paper, Box, Typography, FormControl, FormLabel, Input, Button, Link } from '@mui/material';
+import { Paper, Box, Typography, FormControl, FormLabel, OutlinedInput, Button, Link } from '@mui/material';
 
 function Login () {
   const { setToken } = useContext(AuthContext);
@@ -21,8 +21,9 @@ function Login () {
     }
   }
   return (
-    <Paper sx={{
-      p: 3
+    <Paper variant='outlined' sx={{
+      p: 3,
+      backgroundColor: "#202020"
     }}>
       {error?.error && <div id='error-message'>{`${error.error}: ${error.reason}`}</div>}
       <Typography variant='h4'>Sign In</Typography>
@@ -33,13 +34,21 @@ function Login () {
       }}>
         <FormControl>
           <FormLabel variant='standard' for="username">Username</FormLabel>
-          <Input id="username" />
+          <OutlinedInput id="username" sx={{
+            height: '2.5rem',
+            mt: 1,
+            mb: 2
+          }}/>
         </FormControl>
         <FormControl>
           <FormLabel variant='standard' for="password">Password</FormLabel>
-          <Input id="password" type='password' />
+          <OutlinedInput id="password" type='password' sx={{
+            height: '2.5rem',
+            mt: 1,
+            mb: 2
+          }}/>
         </FormControl>
-        <Button type="submit">Login</Button>
+        <Button variant='contained' type="submit">Login</Button>
       </Box>
       <Link to='/register'>Don't have an account? Sign up here</Link>
     </Paper>
