edb9333e54966dc47c261a8429091e9f5407eb68
scburkha005 <scburkha005@gmail.com>
2025-11-24 14:58:52 -0800

    added basic text and styling

--- src/components/TestingArea.jsx
+++ src/components/TestingArea.jsx
@@ -1,10 +1,23 @@
 import TwitchEmbedChat from "./TwitchEmbedChat";
 import FeatureDropDown from "./FeatureDropDown";
-import { Box } from "@mui/material";
+import { Box, Typography, Divider } from "@mui/material";
 function TestingArea () {
 
   return (
-    <Box>
+    <Box sx={{
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: "center",
+      textAlign: 'center'
+    }}>
+      <Typography variant="h5">Welcome to the Testing Grounds!</Typography>
+      <Typography>If a feature is disabled below, it must be enabled in Account Settings before you are able to test it</Typography>
+      <Divider sx={{
+        borderColor: "lightgrey",
+        width: '95%',
+        mt: ".5rem",
+        mb: "1rem"
+      }}/>
       <FeatureDropDown />
       <TwitchEmbedChat />
     </Box>
