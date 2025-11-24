c19cffb9f97dc2dab13b792e5f74b8d445504aaa
scburkha005 <scburkha005@gmail.com>
2025-11-23 16:06:21 -0800

    Added component for embedded twitch chat

--- src/components/TestingArea.jsx
+++ src/components/TestingArea.jsx
@@ -1,10 +1,11 @@
-
+import TwitchEmbedChat from "./TwitchEmbedChat";
+import { Box } from "@mui/material";
 function TestingArea () {
 
   return (
-    <>
-
-    </>
+    <Box>
+      <TwitchEmbedChat />
+    </Box>
   );
 }
 
---
+++ src/components/TwitchEmbedChat.jsx
@@ -0,0 +1,12 @@
+
+function TwitchEmbedChat () {
+  return (
+    <iframe id="twitch-chat-embed"
+        src="https://www.twitch.tv/embed/ashagni/chat?parent=akenshi-bot.ashagni.live"
+        height="500"
+        width="350">
+    </iframe>
+  );
+}
+
+export default TwitchEmbedChat;
