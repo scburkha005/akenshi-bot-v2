70346bd5ab6e4360c9e70dfcb4832850ddbf1f90
scburkha005 <scburkha005@gmail.com>
2025-11-24 13:30:18 -0800

    changed linked chat to account specifically made for demoing app

--- src/components/TwitchEmbedChat.jsx
+++ src/components/TwitchEmbedChat.jsx
@@ -2,7 +2,7 @@
 function TwitchEmbedChat () {
   return (
     <iframe id="twitch-chat-embed"
-        src="https://www.twitch.tv/embed/ashagni/chat?parent=akenshi-bot.ashagni.live"
+        src="https://www.twitch.tv/embed/akenshibottestinggrounds/chat?parent=akenshi-bot.ashagni.live"
         height="500"
         width="350">
     </iframe>
