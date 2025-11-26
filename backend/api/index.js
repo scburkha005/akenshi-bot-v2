56b2143d7286e424b1c59865b8c2c96a2164fc15
scburkha005 <scburkha005@gmail.com>
2025-11-26 15:15:15 -0800

    fixed typo

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,3 +1,3 @@
 export { default as userRouter } from './routes/users.js';
 export { default as twitchRouter } from './routes/twitch.js';
-export { default as demoRouter } from './routes./demo.js';
+export { default as demoRouter } from './routes/demo.js';
