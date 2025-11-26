c6314d7da4e6e1619066faf3dcfb84fd6b6138da
scburkha005 <scburkha005@gmail.com>
2025-11-26 15:24:42 -0800

    added backend route to toggle gtot mode without chat command of the current user

--- backend/api/routes/demo.js
+++ backend/api/routes/demo.js
@@ -1,4 +1,26 @@
 import express from 'express';
+import { sendMessage } from '../modules/eventsub.js';
+import { updateUser } from '../../db/adapters/users.js';
+import { requireUser } from '../modules/requireUser.js';
 const demoRouter = express.Router();
+
+// PATCH /api/demo/toggleGtotMode
+demoRouter.patch('/toggleGtotMode', requireUser, async (req, res, next) => {
+  try {
+    let broadcasterUsername = req.user.username;
+    let broadcasterId = req.user.twitchUserId;
+    await sendMessage(broadcasterId, `Simulating message received to enter/exit gtot mode`);
+    await updateUser(broadcasterUsername, {
+      botSettings: {
+        gtotModeEnabled: !req.user.botSettings.gtotModeEnabled
+      }
+    });
+    await sendMessage(broadcasterId, !req.user.botSettings.gtotModeEnabled ? `Gtot mode disabled: Deadge` : `Gtot mode enabled: Okayge`);
+
+  } catch (err) {
+    console.log('error while toggling gtot mode for demo');
+    next(err);
+  }
+})
 
 export default demoRouter;
