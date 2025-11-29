cabed70843cd29d56d6eb78e71271a94e02d999a
scburkha005 <scburkha005@gmail.com>
2025-11-29 13:45:52 -0800

    added delete route for clearing firstmessagelogs, added res.send to all routes to prevent server from hanging

--- backend/api/routes/demo.js
+++ backend/api/routes/demo.js
@@ -3,6 +3,8 @@ import { sendMessage } from '../modules/eventsub.js';
 import { updateUser } from '../../db/adapters/users.js';
 import { requireUser } from '../modules/requireUser.js';
 import { triggerRaidEvent } from '../modules/execShell.js';
+import { deleteAllFirstMessageLogsByBroadcasterId } from '../../db/adapters/firstMessage.js';
+import { ResponseStream } from 'openai/lib/responses/ResponseStream.js';
 const { HMAC_SECRET } = process.env;
 const demoRouter = express.Router();
 
@@ -19,6 +21,9 @@ demoRouter.patch('/toggleGtotMode', requireUser, async (req, res, next) => {
     });
     await sendMessage(broadcasterId, !req.user.botSettings.gtotModeEnabled ? `Gtot mode enabled: Okayge` : `Gtot mode disabled: Deadge`);
 
+    res.send({
+      message: "Gtot mode successfully toggled"
+    });
   } catch (err) {
     console.log('error while toggling gtot mode for demo');
     next(err);
@@ -30,10 +35,24 @@ demoRouter.post('/raid', requireUser, async (req, res, next) => {
   try {
     let broadcasterId = req.user.twitchUserId;
     triggerRaidEvent(HMAC_SECRET, broadcasterId)
+    res.send({
+      message: "Raid triggered successfully for demo"
+    });
   } catch (err) {
     console.log('error while simulating raid in demo');
     next(err);
   }
-})
+});
+
+demoRouter.delete('/firstMessageLogs', requireUser, async (req, res, next) => {
+  try {
+    await deleteAllFirstMessageLogsByBroadcasterId(user.broadcasterId);
+    res.send({
+      message: "All first message logs cleared successfully"
+    })
+  } catch (err) {
+    console.log('error while removing all firstmessage_logs')
+  }
+});
 
 export default demoRouter;
