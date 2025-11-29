35b618024c780718e4a7214339a98d646268d1c3
scburkha005 <scburkha005@gmail.com>
2025-11-28 16:14:15 -0800

    modified script to use full path

--- backend/api/shellScripts/twitchRaid.sh
+++ backend/api/shellScripts/twitchRaid.sh
@@ -2,4 +2,4 @@
 twitch_secret="$1"
 broadcaster_id="$2"
 
-twitch event trigger raid -F https://akenshi-bot.ashagni.live/api/twitch/eventsub -s $twitch_secret -t $broadcaster_id
+/home/linuxbrew/.linuxbrew/bin/twitch event trigger raid -F https://akenshi-bot.ashagni.live/api/twitch/eventsub -s $twitch_secret -t $broadcaster_id
